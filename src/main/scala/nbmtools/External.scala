7bc80efc2d932d5975b1557cfdcedc2b6897187a
tkob <ether4@gmail.com>
2015-11-14 17:59:05 +0900

    Add extractor concatWithColon

---
+++ src/main/scala/nbmtools/External.scala
@@ -0,0 +1,10 @@
+package nbmtools
+
+object concatWithColon {
+    def apply(prefix: String, s: String): String = prefix + ":" + s
+    def unapply(s: String): Option[(String, String)] = {
+        val colonPos = s.indexOf(':')
+        if (colonPos < 0) None
+        else Some((s.substring(0, colonPos), s.substring(colonPos + 1)))
+    }
+}
