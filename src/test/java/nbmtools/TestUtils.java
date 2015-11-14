e49226bae566f12dea1b8b3a619a45fac40408b9
tkob <ether4@gmail.com>
2015-11-14 23:48:26 +0900

    Add uris method

--- src/test/java/nbmtools/TestUtils.java
+++ src/test/java/nbmtools/TestUtils.java
@@ -2,7 +2,11 @@ package nbmtools;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
+import scala.collection.JavaConversions;
 import yokohama.unit.annotations.As;
 
 @As
@@ -10,5 +14,11 @@ public class TestUtils {
     @As
     public static InputStream asInputStream(String s) throws IOException {
         return IOUtils.toInputStream(s, "UTF-8");
+    }
+
+    public static scala.collection.immutable.List<URI> uris(List<String> urls) {
+        List<URI> uris =
+                urls.stream().map(URI::create).collect(Collectors.toList());
+        return JavaConversions.asScalaBuffer(uris).toList();
     }
 }
