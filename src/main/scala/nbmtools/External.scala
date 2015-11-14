900e131a2badead8521a2f25b35590ae4bba2ab0
tkob <ether4@gmail.com>
2015-11-14 21:21:14 +0900

    Add External.from method

--- src/main/scala/nbmtools/External.scala
+++ src/main/scala/nbmtools/External.scala
@@ -1,6 +1,8 @@
 package nbmtools
 
+import java.io.InputStream
 import java.net.URI
+import scala.io.BufferedSource
 
 case class External(crc: Option[Long], urls: List[URI])
 
@@ -10,5 +12,20 @@ object concatWithColon {
         val colonPos = s.indexOf(':')
         if (colonPos < 0) None
         else Some((s.substring(0, colonPos), s.substring(colonPos + 1)))
+    }
+}
+
+object External {
+    def from(is: InputStream): External = {
+        val lines = new BufferedSource(is).getLines
+        lines.foldRight(External(None, Nil)) {
+            case (concatWithColon("CRC", crc), External(None, urls)) =>
+                External(Some(crc.toLong), urls)
+            case (concatWithColon("CRC", crc), External(Some(_), urls)) =>
+                sys.error("duplicate CRC in external file")
+            case (concatWithColon("URL", url), External(crc, urls)) =>
+                External(crc, (new URI(url)::urls))
+            case (_, external) => external
+        }
     }
 }
