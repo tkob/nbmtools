a09faa88be4b083d234c628377268c91919123f1
tkob <ether4@gmail.com>
2015-11-15 00:49:11 +0900

    Add createZipFile utility method

--- src/test/java/nbmtools/TestUtils.java
+++ src/test/java/nbmtools/TestUtils.java
@@ -1,10 +1,13 @@
 package nbmtools;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.zip.ZipOutputStream;
 import org.apache.commons.io.IOUtils;
 import scala.collection.JavaConversions;
 import yokohama.unit.annotations.As;
@@ -20,5 +23,18 @@ public class TestUtils {
         List<URI> uris =
                 urls.stream().map(URI::create).collect(Collectors.toList());
         return JavaConversions.asScalaBuffer(uris).toList();
+    }
+
+    public static void createZipFile(File file, List<ZipEntry> entries)
+            throws IOException {
+        try(FileOutputStream fos = new FileOutputStream(file);
+                ZipOutputStream zos = new ZipOutputStream(fos)) {
+            for (ZipEntry entry : entries) {
+                if (entry instanceof DirEntry) continue;
+                FileEntry fileEntry = (FileEntry)entry;
+				zos.putNextEntry(fileEntry.getZipEntry());
+                IOUtils.copy(fileEntry.getInputStream(), zos);
+            }
+        }
     }
 }
