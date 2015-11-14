02add8a586473a6025cdd591fbafe7cd5dfa1709
tkob <ether4@gmail.com>
2015-11-14 13:24:15 +0900

    Add nbmtools.ZipEntry

---
+++ src/main/scala/nbmtools/ZipEntry.scala
@@ -0,0 +1,39 @@
+package nbmtools
+
+import nbmtools.ZipUtils.Entry
+import org.apache.commons.io.IOUtils
+
+sealed trait ZipEntry
+
+case class DirEntry(
+    name: String, time: Long, comment: String, extra: Seq[Byte])
+    extends ZipEntry
+
+case class FileEntry(
+    name: String, content: Seq[Byte], time: Long, comment: String, extra: Seq[Byte])
+    extends ZipEntry
+
+object ZipEntry {
+    def of(entry: Entry): ZipEntry = {
+        entry match {
+            case (entry: ZipUtils.DirEntry) => {
+                    val zipEntry = entry.getZipEntry
+                    val name = zipEntry.getName
+                    val time = zipEntry.getTime
+                    val comment = zipEntry.getComment
+                    val extra: Seq[Byte] = zipEntry.getExtra
+                    DirEntry(name, time, comment, extra)
+            }
+            case (entry: ZipUtils.FileEntry) => {
+                    val zipEntry = entry.getZipEntry
+                    val name = zipEntry.getName
+                    val time = zipEntry.getTime
+                    val comment = zipEntry.getComment
+                    val extra: Seq[Byte] = zipEntry.getExtra
+                    val content: Seq[Byte] =
+                        IOUtils.toByteArray(entry.getInputStream)
+                    FileEntry(name, content, time, comment, extra)
+            }
+        }
+    }
+}
