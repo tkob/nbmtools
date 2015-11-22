3d6224fe286052871936717ceebe49224bd70978
tkob <ether4@gmail.com>
2015-11-22 21:43:23 +0900

    Implement Internalize

--- src/main/scala/nbmtools/Internalize.scala
+++ src/main/scala/nbmtools/Internalize.scala
@@ -1,12 +1,44 @@
 package nbmtools
 
+import java.io.Closeable
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.PrintStream
+import java.net.URL
+import org.apache.commons.io.IOUtils
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
 
 class Internalize extends Command {
+    def openIn(s: String) =
+        Try(new URL(s)) match {
+            case Success(url) => url.openStream
+            case Failure(_) => new FileInputStream(s)
+        }
+
+    private def using[T <: Closeable, U](resource: T) ( block: T => U ) =
+        try { block } finally { resource.close }
+
     override def run(
         in: InputStream, out: PrintStream, err: PrintStream, args: String*) = {
-        System.err.println("TODO")
-        Command.EXIT_SUCCESS
+        val status: Try[Int] = for {
+            fromName <- Try(args(0))
+            toName <- Try(args(1))
+        } yield {
+            using(new NbmInputStream(openIn(fromName))) { fromStream =>
+            using(new FileOutputStream(toName)) { toStream =>
+                IOUtils.copy(fromStream, toStream)
+            }}
+            Command.EXIT_SUCCESS
+        }
+       status match {
+            case Success(status) => status
+            case Failure(_)  => {
+                    err.println("too few arguments")
+                    Command.EXIT_FAILURE
+            }
+        }
     }
 }
