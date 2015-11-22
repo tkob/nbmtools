c72b06c7e10638dede5dd2dd4ce47417e3fdcdbf
tkob <ether4@gmail.com>
2015-11-22 19:59:29 +0900

    Implement Main

--- src/main/scala/nbmtools/Main.scala
+++ src/main/scala/nbmtools/Main.scala
@@ -1,8 +1,14 @@
 package nbmtools
 
 object Main {
+    private val commands = Map("internalize" -> new Internalize());
+
     def main(args: Array[String]) {
-        // TODO
-        System.out.println("hello")
+        val status: Option[Int] = for {
+            cmdName <- args.headOption
+            cmd <- commands.get(cmdName)
+        } yield cmd.run(System.in, System.out, System.err, args:_*)
+        
+        System.exit(status.getOrElse(Command.EXIT_FAILURE))
     }
 }
