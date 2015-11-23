f6c67499e028ef88a35e6e458b05d9532950717f
tkob <ether4@gmail.com>
2015-11-23 18:51:47 +0900

    Print usage unless valid subcommand name is given

--- src/main/scala/nbmtools/Main.scala
+++ src/main/scala/nbmtools/Main.scala
@@ -3,12 +3,27 @@ package nbmtools
 object Main {
     private val commands = Map("internalize" -> new Internalize());
 
+    private def usage() {
+        System.err.println("""usage: nbmtools <subcommand> [<args>]
+                             |
+                             |subcommands are:""".stripMargin)
+        commands.keys.toList.sorted.foreach { key =>
+            System.err.println("    " + key)
+        }
+    }
+
     def main(args: Array[String]) {
         val status: Option[Int] = for {
             cmdName <- args.headOption
             cmd <- commands.get(cmdName)
         } yield cmd.run(System.in, System.out, System.err, args.tail:_*)
         
-        System.exit(status.getOrElse(Command.EXIT_FAILURE))
+        status match {
+            case Some(status) => System.exit(status)
+            case None => {
+                    usage()
+                    System.exit(Command.EXIT_FAILURE)
+            }
+        }
     }
 }
